e0a93623482f73fbd2e7fdd21638bfe17f385607
kdawson133 <kdawson133@startmail.com>
2024-11-30 15:05:33 +1000

    updated

--- ollama/ollama-modelfiles/regenerate.sh
+++ ollama/ollama-modelfiles/regenerate.sh
@@ -2,8 +2,9 @@
 ollama pull dolphin-llama3
 ollama pull dolphin-mistral
 ollama pull dolphin-phi3
+ollama pull mistral-nemo
 
 ollama create magnus -f magnus.modelfile
 ollama create morpheus -f morpheus.modelfile
 ollama create neo -f neo.modelfile
-
+ollama create nemo -f nemo.modelfile
