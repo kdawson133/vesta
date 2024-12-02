6a3728138f4e4c4c924c31ccaf34cfcc090d56e2
kdawson133 <kdawson133@startmail.com>
2024-12-02 18:56:10 +1000

    updated

--- ollama/ollama-modelfiles/regenerate.sh
+++ ollama/ollama-modelfiles/regenerate.sh
@@ -3,8 +3,10 @@ ollama pull llama3.2:3b
 ollama pull notus
 ollama pull hf.co/arcee-ai/SuperNova-Medius-GGUF
 ollama pull mistral-nemo
+ollama pull qwen2.5:14b
 
 ollama create magnus -f magnus.modelfile
 ollama create morpheus -f morpheus.modelfile
 ollama create neo -f neo.modelfile
 ollama create nemo -f nemo.modelfile
+ollama create zen -f zen.modelfile
