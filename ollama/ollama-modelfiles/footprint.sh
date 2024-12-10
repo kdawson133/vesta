5ed6e32be8d90de86cfb923e4cb386f64b65b31e
kdawson133 <kdawson133@startmail.com>
2024-12-10 16:23:49 +1000

    updated

--- ollama/ollama-modelfiles/footprint.sh
+++ ollama/ollama-modelfiles/footprint.sh
@@ -1,5 +1,21 @@
-
+#!/bin/sh
 ollama run axel why is the sky blue 
 ollama ps > mem.txt
 ollama run beau why is the sky blue
+ollama ps >> mem.txt
+ollama run cody why is the sky blue
+ollama ps >> mem.txt
+ollama run dean why is the sky blue
+ollama ps >> mem.txt
+ollama run enzo why is the sky blue
+ollama ps >> mem.txt
+ollama run fred why is the sky blue
+ollama ps >> mem.txt
+ollama run glen why is the sky blue
+ollama ps >> mem.txt
+ollama run hugo why is the sky blue
+ollama ps >> mem.txt
+ollama run ivan why is the sky blue
+ollama ps >> mem.txt
+ollama run jack why is the sky blue
 ollama ps >> mem.txt
