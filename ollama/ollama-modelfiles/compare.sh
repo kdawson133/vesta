3a6a34b6845caf5fb47daf5733850c9927766445
kdawson133 <kdawson133@startmail.com>
2024-12-13 16:32:42 +1000

    updated

--- ollama/ollama-modelfiles/compare.sh
+++ ollama/ollama-modelfiles/compare.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
+query='Create a blog post about the Roswell Incident in 1947'
 echo '-axel----------------------------------------' > comp.txt
-ollama run axel Who was Alger Hiss? >> comp.txt
+ollama run axel $query >> comp.txt
 echo '-beau----------------------------------------' >> comp.txt
-ollama run beau Who was Alger Hiss? >> comp.txt
+ollama run beau $query >> comp.txt
 echo '-cody----------------------------------------' >> comp.txt
-ollama run cody Who was Alger Hiss? >> comp.txt
+ollama run cody $query >> comp.txt
 echo '-dean----------------------------------------' >> comp.txt
-ollama run dean Who was Alger Hiss? >> comp.txt
+ollama run dean $query >> comp.txt
 echo '-enzo----------------------------------------' >> comp.txt
-ollama run enzo Who was Alger Hiss? >> comp.txt
+ollama run enzo $query >> comp.txt
 echo '-fred----------------------------------------' >> comp.txt
-ollama run fred Who was Alger Hiss? >> comp.txt
+ollama run fred $query >> comp.txt
 echo '-glen----------------------------------------' >> comp.txt
-ollama run glen Who was Alger Hiss? >> comp.txt
+ollama run glen $query >> comp.txt
 echo '-hugo----------------------------------------' >> comp.txt
-ollama run hugo Who was Alger Hiss? >> comp.txt
+ollama run hugo $query >> comp.txt
 echo '-ivan----------------------------------------' >> comp.txt
-ollama run ivan Who was Alger Hiss? >> comp.txt
+ollama run ivan $query >> comp.txt
 echo '-jack----------------------------------------' >> comp.txt
-ollama run jack Who was Alger Hiss? >> comp.txt
+ollama run jack $query >> comp.txt
 echo '-****----------------------------------------' >> comp.txt
 cat comp.txt
